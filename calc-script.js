563b8fabfd68468ad6072fbe7909349ee6ec6a43
ellis <elrestea@gmail.com>
2026-02-05 03:31:38 +0000

    finish delete button logic, start plus/minus logic

--- calc-script.js
+++ calc-script.js
@@ -7,7 +7,10 @@ let allInputs = [];
 let operators = ['+', '-', '×', '÷', '%','='];
 let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let funcBtns = ['AC', '⌫'];
+let plusMinusBtn = '±';
 let isStartingNum = true;
+let notNegative = true;
+let firstIntIndexes = [];
     
 
 let decimalAdded = false;
@@ -20,18 +23,34 @@ buttons.forEach(button => {
         lastInput = button.textContent; 
         // lastInput is the value of the button that was clicked
 
-        if (button.id === 'Ac-btn') {
+        function reset(){
             allInputs = [];
             lastInput = '';
             display.textContent = '0';
             isStartingNum = true;
             decimalAdded = false;
+            firstIntIndexes = [];
             return;
+        }
+
+        if (button.id === 'Ac-btn') {
+           reset();
         }
         // AC button logic
         
 
+
+
+
         if (button.id === 'delete-btn') {
+
+
+            if (operators.includes(allInputs[allInputs.length -1])) {
+                isStartingNum = false;
+            }
+            // if last displayed that was deleted is an operator, set isStartingNum to true
+
+            
             
             if (allInputs.length === 0) {
                 return;
@@ -41,6 +60,13 @@ buttons.forEach(button => {
             display.textContent = display.textContent.slice(0, -1);
             allInputs.pop();
 
+
+            if (!allInputs[firstIntIndexes[firstIntIndexes.length -1]]){
+                firstIntIndexes.pop(firstIntIndexes[firstIntIndexes.length -1]);
+
+            }
+
+
             if (!allInputs.includes('.')) {
                 decimalAdded = false;
 
@@ -48,20 +74,10 @@ buttons.forEach(button => {
             // if decimal was deleted, allow adding another decimal
 
             if (allInputs.length === 0) {
-                allInputs = [];
-                lastInput = '';
-                display.textContent = '0';
-                isStartingNum = true;
-                return;
+                reset();
             }
             // if all inputs is empty after deletion, reset everything
 
-
-            console.log(allInputs.length);
-            if (operators.includes(allInputs[allInputs.length -1])) {
-                isStartingNum = true;
-            }
-            // if last displayed is an operator, set isStartingNum to true
 
             return;
 
@@ -88,17 +104,41 @@ buttons.forEach(button => {
             decimalAdded = false;
         }
         // decimals can only be added once per number
+
+
+
+        
+        if (lastInput === plusMinusBtn) {
+            
+                if (notNegative && !isStartingNum) {
+                    if(!allInputs.some(input => operators.includes(input))){
+                        display.textContent = "-" + display.textContent;
+                        allInputs.unshift("-");
+                        notNegative = !notNegative;
+                    }
+                if (notNegative){
+                        display.textContent = display.textContent.slice(0, firstIntIndexes[firstIntIndexes.length -1]) + "-" + display.textContent.slice(firstIntIndexes[firstIntIndexes.length -1]);
+                        allInputs.splice(firstIntIndexes[firstIntIndexes.length -1], 0,"-")
+                        notNegative = !notNegative;
+                    }
+                }
+                if(!notNegative){
+
+                }
+
+            }
+
         
         
         
         
           
         if (operators.includes(lastInput)) {
-            console.log('Last input was an operator');
+
             isStartingNum = true;
 
             if (operators.includes(allInputs[allInputs.length -1]) ) {
-                console.log('Replacing last operator with new operator');
+
                 display.textContent = display.textContent.slice(0, -1);
                 allInputs.pop();
                 
@@ -110,6 +150,7 @@ buttons.forEach(button => {
         
         // prevents entering two operators in a row
 
+        // operators logic
         
 
          
@@ -121,18 +162,23 @@ buttons.forEach(button => {
                 return;
             }
 
+            allInputs.push(lastInput);
+
             //prevents first input being 0 
 
             if (display.textContent === '0'){
                 display.textContent = lastInput;
                 isStartingNum = false;
+                firstIntIndexes.push(allInputs.length -1);
             }
 
             // replaces initial 0 with first number 1-9 input
 
             else if (isStartingNum) {
+                
                 display.textContent += lastInput;
                 isStartingNum = false;
+                firstIntIndexes.push(allInputs.length -1);  
             }
 
             // after an operator, allows all numbers to be added
@@ -143,15 +189,19 @@ buttons.forEach(button => {
 
             // all other number inputs
 
-            allInputs.push(lastInput);
+            
 
 
         }
         // numbers logic
 
 
+        // ADD IF RESULT IS NEGATIVE , is NEGATIVE = TRUE !!MUST BE BEFORE +- LOGIC
+        // add if any num button is pressed, decimal or plusminus, ac -> C
+
 
         console.log('Button clicked (last input):', lastInput);
         console.log('All inputs so far:', allInputs);
+        console.log("all first int indexes ", firstIntIndexes);
     });
 });
